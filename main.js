86edd70e57e0d5aa2ac14c523408f2cecd473962
Anonymous <xolpolatovshohjahhon5@gmail.com>
2021-06-26 11:48:11 +0500

    koryo

---
+++ main.js
@@ -0,0 +1,19 @@
+const body = document.getElementsByTagName("body");
+const loader = document.getElementById("loader");
+
+setTimeout(() => {
+    const loader = document.getElementById("loader");
+   
+    
+      loader.classList.add("d-none");
+},2000)
+
+
+setTimeout(() => {
+    // const loader = document.getElementById("loader");
+const body = document.getElementsByTagName("body")[0];
+ loader.classList.add("")
+  
+    
+    
+},2000)
